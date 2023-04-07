2e5e1a64ca784d4616d05443bd02fc6e251c194e
Lucas Luna <lucassilvavg9@gmail.com>
2023-04-07 19:59:23 -0300

    Incluindo aula 5

---
+++ PDO_PHP/aula5.php
@@ -0,0 +1,62 @@
+<?php
+
+    // Trabalhando com INNER JOIN E ON para fazer relacionamento entre tabelas
+
+    // fazendo conexão
+    $pdo = new PDO('mysql:host=localhost;dbname=pdo_jedai','root','');
+
+    // Relacionando colunas das tabelas posts e categorias
+    $sql = $pdo->prepare("SELECT * FROM `tb_posts` INNER JOIN `tb_categoria` ON `tb_posts`.`categoria_id` = 
+    `tb_categoria`.`id`");
+    $sql->execute();
+    $info3 = $sql->fetchAll();
+
+
+    foreach($info3 as $key => $value){
+        echo 'Titulo -> '.$value['titulo'];
+        echo '<br/>';
+        echo 'Noticia -> '.$value['conteudo'];
+        echo '<hr>';
+    }
+
+
+
+
+
+
+
+   
+
+   //Selecionando todo o conteudo do tabela posts
+
+   $sql = $pdo->prepare(" SELECT * FROM tb_posts");
+    $sql->execute();
+    $info1 = $sql->fetchAll();
+
+    foreach($info1 as $key => $value){
+        echo 'Titulo -> '.$value['titulo'];
+        echo '</br>';
+        echo 'Titulo -> '.$value['conteudo'];
+    } 
+    
+    
+
+
+     
+     // Selecionando conteudo da tabela via GET
+     
+     $sql = $pdo->prepare("SELECT * FROM tb_posts WHERE categoria_id = ?");
+    $sql->execute(array($_GET['categoria_id']));
+    $info2 = $sql->fetchAll();
+
+    // para recuperar valor passado pelo get, digite na url, PDO_PHP/aula5.php/index.php?categoria_id=2
+    foreach($info2 as $key => $value){
+        echo 'Titulo -> '.$value['titulo'];
+        echo '<br/>';
+        echo 'Noticia -> '.$value['conteudo'];
+        echo '<hr>';
+    }
+
+    
+
+?>
